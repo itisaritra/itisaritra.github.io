3ff760f6ea248d00f4f7a47a02ed1cedcadbf837
itisaritra <116426899+itisaritra@users.noreply.github.com>
2023-04-19 00:28:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", () =>{
     document.querySelector(".page-loader").classList.add("fade-out");
     setTimeout(() =>{
         document.querySelector(".page-loader").style.display = "none";
-    },300);
+    },100);
 });
 
 
